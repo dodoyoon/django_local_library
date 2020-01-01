1f7bbb1a6c8814918f47248d24ee7857f8f064b4
greentealemonade <dodo4.0@Kateui-MacBookPro.local>
2020-01-01 17:46:18 +0900

    change at settings.py - ALLOWED_HOST

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '^6&x*2k^7hqnunbz2tlm2p7##9lp(j
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysterious-headland-14357.herokuapp.com']
 
 
 # Application definition
